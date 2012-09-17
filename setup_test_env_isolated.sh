5c982a6bb2b9b9d7b410a63c3f4003f2a28b8808
Adrien Devresse <adevress@cern.ch>
2012-09-17 11:54:19 +0000

    - minor fix for the gfal 2.0 etics tests [ticket:323]

--- setup_test_env_isolated.sh
+++ setup_test_env_isolated.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 export GFAL_CONFIG_DIR=`pwd`/test/conf_test/
+
+
+## export etics needed configs 
+if [ -f "/etc/grid-security/test-user/test_user_501_cert.pem" ]; then
+export USER_CERT="/etc/grid-security/test-user/test_user_501_cert.pem"
+export USER_KEY="/etc/grid-security/test-user/test_user_501_key.pem"
+else
+export USER_CERT="/root/user_certificates/test_user_501_cert.pem"
+export USER_KEY="/root/user_certificates/test_user_501_key.pem"
+fi
+
 $@
