f4dd91a9e61a3b432e9a3e80745bff138dd62b30
Adrien Devresse <adevress@cern.ch>
2013-01-24 11:06:01 +0000

    - add un test for cancel logic - LCGUTIL-157

---
+++ test/unit/cancel/cancel_tests.cpp
@@ -0,0 +1,48 @@
+
+
+
+
+#include <gfal_api.h>
+#include <gtest/gtest.h>
+
+
+TEST(gfalCancel, test_cancel_simple){
+    GError* tmp_err=NULL;
+    gfal2_context_t c = gfal2_context_new(&tmp_err);
+    ASSERT_TRUE(c != NULL);
+    gfal2_cancel(c);
+    gfal2_cancel(c);
+    gfal2_cancel(c);
+    gfal2_context_free(c);
+}
+
+void gfal_cancel_hook_cb_s(gfal2_context_t context, void* userdata){
+    int* p = (int*)userdata;
+    *p += 1;
+    (void) gfal_version();
+
+}
+
+TEST(gfalGlobal, testCancelCallback){
+    GError* tmp_err=NULL;
+    gfal2_context_t c = gfal2_context_new(&tmp_err);
+    ASSERT_TRUE(c != NULL);
+
+    int i=0,res ;
+    // add callback7
+    gfal_cancel_token_t tok = gfal2_register_cancel_callback(c, &gfal_cancel_hook_cb_s, &i);
+    ASSERT_TRUE( tok != NULL);
+    res= gfal2_cancel(c);
+    ASSERT_EQ(res, 0);
+    ASSERT_EQ(i, 1);
+    gfal2_remove_cancel_callback(c, tok);
+    res= gfal2_cancel(c);
+    ASSERT_EQ(res, 0);
+    ASSERT_EQ(i, 1);
+
+    res= gfal2_cancel(c);
+    ASSERT_EQ(res, 0);
+    gfal2_context_free(c);
+}
+
+
--- test/unit/global/global_test.cpp
+++ test/unit/global/global_test.cpp
@@ -12,5 +12,14 @@ TEST(gfalGlobal, test_verbose){
     ASSERT_TRUE(r==GFAL_VERBOSE_DEBUG);
 }
 
+TEST(gfalGlobal, testLoad){
+    GError* tmp_err=NULL;
+    gfal2_context_t c = gfal2_context_new(&tmp_err);
+    ASSERT_EQ(tmp_err, (GError*)NULL);
+    ASSERT_TRUE(c != NULL);
+    gfal2_context_free(c);
+    c = NULL;
+    gfal2_context_free(c);
+}
 
 
