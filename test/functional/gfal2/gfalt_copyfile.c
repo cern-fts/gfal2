302b73257c88399b0739e71cc6d103e99b866d37
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-01-25 08:17:47 +0000

    Print string representation of source/dest/both

--- test/functional/gfal2/gfalt_copyfile.c
+++ test/functional/gfal2/gfalt_copyfile.c
@@ -8,6 +8,19 @@
 #include <transfer/gfal_transfer.h>
 
 
+void event_callback(const gfalt_event_t e, gpointer user_data)
+{
+  static const char* side_str[] = {"SRC",
+                                   "DST",
+                                   "BTH"};
+
+  printf("[%s:%ld] %s\t%s\t%s\n",
+         side_str[e->side], e->timestamp,
+         g_quark_to_string(e->domain), g_quark_to_string(e->stage),
+         e->description);
+}
+
+
 int main(int argc, char** argv){
 	if( argc <3 ){
 		printf(" Usage %s [src_url] [dst_url] \n",argv[0]);
@@ -25,8 +38,15 @@ int main(int argc, char** argv){
 	gfal2_set_opt_string(handle, "bdii",
                                     "LCG_GFAL_INFOSYS", "certtb-bdii-top.cern.ch:2170", &tmp_err);	 
 	 
+	// Params
+	gfalt_params_t params = gfalt_params_handle_new(NULL);
+	gfalt_set_replace_existing_file(params, TRUE, NULL);
+
+	// Register callback
+	gfalt_set_event_callback(params, event_callback, &tmp_err);
+
 	 // begin copy
-    if(  gfalt_copy_file(handle, NULL, argv[1], argv[2], &tmp_err)   != 0){
+    if(  gfalt_copy_file(handle, params, argv[1], argv[2], &tmp_err)   != 0){
          printf(" error while the file transfer %d : %s.\n", tmp_err->code,tmp_err->message);
          gfal2_context_free(handle);
 		 return -1;		
