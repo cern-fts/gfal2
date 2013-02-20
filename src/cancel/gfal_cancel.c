49b25ba5582762129427b8f6add920916af86416
Adrien Devresse <adevress@cern.ch>
2013-02-20 17:23:01 +0000

    - protect cancel logic again recusrive calls LCGUTIL-109

--- src/cancel/gfal_cancel.c
+++ src/cancel/gfal_cancel.c
@@ -35,6 +35,8 @@
 
 int gfal2_cancel(gfal2_context_t context){
     g_assert(context);
+    if(context->cancel == TRUE) // avoid recursive calls
+        return 0;
     g_mutex_lock(context->mux_cancel);
     const int n_cancel = g_atomic_int_get(&(context->running_ops));
     context->cancel = TRUE;
