568d8c7930064d9f94e83b9fc434fa22da34a1bd
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-01-29 16:37:07 +0000

    LCGUTIL-334: Remove first, check later if the error was ENOENT

--- src/plugins/srm/gfal_srm_stat.c
+++ src/plugins/srm/gfal_srm_stat.c
@@ -51,29 +51,37 @@ int gfal_srm_statG(plugin_handle ch, const char* surl, struct stat* buf, GError*
 	gfal_srmv2_opt* opts = (gfal_srmv2_opt*) ch;
 	gfal_srm_construct_key(surl, GFAL_SRM_LSTAT_PREFIX, key_buff, GFAL_URL_MAX_LEN);
 	
-	if (gsimplecache_take_one_kstr(opts->cache, key_buff, buf) ==0){
-		gfal_log(GFAL_VERBOSE_DEBUG, " srm_statG -> value taken from the cache");
-		ret = 0;
-	}else{
-		ret =gfal_srm_determine_endpoint(opts, surl, full_endpoint, GFAL_URL_MAX_LEN, &srm_type,   &tmp_err);
-		if( ret >=0 ){
-			if(srm_type == PROTO_SRMv2){
-				gfal_log(GFAL_VERBOSE_VERBOSE, "   [gfal_srm_statG] try to stat file %s", surl);				
-				ret = gfal_statG_srmv2_internal(opts, buf, full_endpoint, surl, &tmp_err);
-                if( ret ==0){
-                   gfal_log(GFAL_VERBOSE_TRACE, "   [gfal_srm_statG] store %s stat info in cache", surl);
-                   gfal_srm_cache_stat_add(ch, surl, buf);
+	if (gsimplecache_take_one_kstr(opts->cache, key_buff, buf) == 0) {
+        gfal_log(GFAL_VERBOSE_DEBUG,
+                " srm_statG -> value taken from the cache");
+        ret = 0;
+    }
+    else {
+        ret = gfal_srm_determine_endpoint(opts, surl, full_endpoint,
+                GFAL_URL_MAX_LEN, &srm_type, &tmp_err);
+        if (ret >= 0) {
+            if (srm_type == PROTO_SRMv2) {
+                gfal_log(GFAL_VERBOSE_VERBOSE, "   [gfal_srm_statG] try to stat file %s", surl);
+                ret = gfal_statG_srmv2_internal(opts, buf, full_endpoint, surl,
+                        &tmp_err);
+                if (ret == 0) {
+                    gfal_log(GFAL_VERBOSE_TRACE, "   [gfal_srm_statG] store %s stat info in cache", surl);
+                    gfal_srm_cache_stat_add(ch, surl, buf);
                 }
-			}else if (srm_type == PROTO_SRM){
-                g_set_error(&tmp_err, gfal2_get_plugin_srm_quark(), EPROTONOSUPPORT, "support for SRMv1 is removed in 2.0, failure");
-				ret = -1;
-			}else {
-                g_set_error(&tmp_err, gfal2_get_plugin_srm_quark(), EPROTONOSUPPORT, "Unknow version of the protocol SRM , failure");
-				ret = -1;			
-			}
-			
-		}
-	}
+            }
+            else if (srm_type == PROTO_SRM) {
+                g_set_error(&tmp_err, gfal2_get_plugin_srm_quark(),
+                        EPROTONOSUPPORT, "support for SRMv1 is removed in 2.0, failure");
+                ret = -1;
+            }
+            else {
+                g_set_error(&tmp_err, gfal2_get_plugin_srm_quark(),
+                        EPROTONOSUPPORT, "Unknow version of the protocol SRM , failure");
+                ret = -1;
+            }
+
+        }
+    }
 	if(tmp_err)
 		g_propagate_prefixed_error(err, tmp_err, "[%s]", __func__);
 	return ret;
