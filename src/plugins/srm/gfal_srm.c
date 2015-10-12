7a61b57ee565a5f88ee1b7213ca06c07e6f443a4
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-10-12 15:19:09 +0200

    DMC-750: Respect GLOBUS_THREAD_MODEL environment

--- src/plugins/srm/gfal_srm.c
+++ src/plugins/srm/gfal_srm.c
@@ -42,7 +42,8 @@
 __attribute__((constructor))
 static void globus_setup(void)
 {
-    globus_thread_set_model("pthread");
+    if (!getenv("GLOBUS_THREAD_MODEL"))
+        globus_thread_set_model("pthread");
 
     globus_module_activate(GLOBUS_GSI_GSS_ASSIST_MODULE);
     globus_module_activate(GLOBUS_GSI_GSSAPI_MODULE);
