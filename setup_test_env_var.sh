d8286a391c46f3a3b127604128aec59a29e28b2a
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-12-14 16:44:35 +0100

    Add coverage.sh
    Remove old scripts

--- setup_test_env_var.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LFC_HOST=cvitblfc1.cern.ch
-export LCG_GFAL_INFOSYS=certtb-bdii-top.cern.ch:2170
-export GFAL_PLUGIN_DIR=`pwd`/plugins/
-export GFAL_CONFIG_DIR=`pwd`/../dist/etc/gfal2/
-export LD_LIBRARY_PATH=`pwd`/src:$LD_LIBRARY_PATH
-$@
