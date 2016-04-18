0d394341e67a08784b69a030ef7cc0fad72779f7
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2016-04-18 09:46:29 +0200

    Add test for gfal2_get_plugin_names

--- test/unit/global/global_test.cpp
+++ test/unit/global/global_test.cpp
@@ -4,63 +4,74 @@
 #include <gtest/gtest.h>
 
 
-TEST(gfalGlobal, testVerbose){
+TEST(gfalGlobal, testVerbose)
+{
     gfal2_log_set_level(G_LOG_LEVEL_INFO);
     GLogLevelFlags r = gfal2_log_get_level();
     ASSERT_EQ(G_LOG_LEVEL_INFO, r);
 }
 
 
-TEST(gfalGlobal, testLoad){
-    GError* tmp_err = NULL;
+TEST(gfalGlobal, testLoad)
+{
+    GError *tmp_err = NULL;
     gfal2_context_t c = gfal2_context_new(&tmp_err);
     if (tmp_err)
         printf("%s\n", tmp_err->message);
     ASSERT_EQ(NULL, tmp_err);
-    ASSERT_NE((void*)NULL, c);
+    ASSERT_NE((void *) NULL, c);
     gfal2_context_free(c);
     c = NULL;
     gfal2_context_free(c);
 }
 
 
-TEST(gfalGlobal, urlParsing){
-    GError* tmp_err = NULL;
-    const char* url = "gsiftp://dcache-door-desy09.desy.de:2811/pnfs/desy.de/dteam/gfal2-tests/testread0011";
-    const char* bad_url = "bob the sponge:";
-    char buffer[GFAL_URL_MAX_LEN] = { 0 };
+TEST(gfalGlobal, urlParsing)
+{
+    GError *tmp_err = NULL;
+    const char *url = "gsiftp://dcache-door-desy09.desy.de:2811/pnfs/desy.de/dteam/gfal2-tests/testread0011";
+    const char *bad_url = "bob the sponge:";
+    char buffer[GFAL_URL_MAX_LEN] = {0};
     int ret = gfal2_hostname_and_port_from_uri(url, buffer, GFAL_URL_MAX_LEN, &tmp_err);
     ASSERT_EQ(0, ret);
     ASSERT_STREQ("dcache-door-desy09.desy.de:2811", buffer);
 
     ret = gfal2_hostname_from_uri(bad_url, buffer, GFAL_URL_MAX_LEN, &tmp_err);
     ASSERT_GT(0, ret);
-    ASSERT_NE((void*)NULL, tmp_err);
+    ASSERT_NE((void *) NULL, tmp_err);
 }
 
-static gboolean test_plugin_url(plugin_handle plugin_data, const char* url,
-        plugin_mode operation, GError** err)
+
+static const char *test_plugin_get_name(void)
+{
+    return "TEST PLUGIN";
+}
+
+
+static gboolean test_plugin_url(plugin_handle plugin_data, const char *url,
+    plugin_mode operation, GError **err)
 {
     return strncmp(url, "test://", 7) == 0 && operation == GFAL_PLUGIN_STAT;
 }
 
 
-static int test_plugin_stat(plugin_handle plugin_data , const char* url, struct stat *buf, GError** err)
+static int test_plugin_stat(plugin_handle plugin_data, const char *url, struct stat *buf, GError **err)
 {
     buf->st_mode = 12345;
     return 0;
 }
 
 
-TEST(gfalGlobal, loadPlugin)
+TEST(gfalGlobal, registerPlugin)
 {
-    GError* tmp_err = NULL;
+    GError *tmp_err = NULL;
     gfal2_context_t c = gfal2_context_new(&tmp_err);
-    ASSERT_NE((void*)NULL, c);
+    ASSERT_NE((void *) NULL, c);
 
     gfal_plugin_interface test_plugin;
     memset(&test_plugin, 0, sizeof(test_plugin));
 
+    test_plugin.getName = test_plugin_get_name;
     test_plugin.check_plugin_url = test_plugin_url;
     test_plugin.statG = test_plugin_stat;
 
@@ -74,6 +85,14 @@ TEST(gfalGlobal, loadPlugin)
     ASSERT_EQ(0, ret);
 
     ASSERT_EQ(12345, st.st_mode);
+
+    gchar **plugins = gfal2_get_plugin_names(c);
+    int found = 0;
+    for (int i = 0; plugins[i] != NULL; ++i) {
+        found += (strncmp(plugins[i], "TEST PLUGIN", 11) == 0);
+    }
+    ASSERT_EQ(found, 1);
+    g_strfreev(plugins);
 
     gfal2_context_free(c);
 }
