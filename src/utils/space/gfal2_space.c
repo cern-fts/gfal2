797f88bed1075301a74154fd8b545aed58954bb4
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2016-09-14 15:50:48 +0200

    DMC-885: Factor out space report generation

---
+++ src/utils/space/gfal2_space.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) CERN 2016
+ *
+ * Copyright (c) Members of the EMI Collaboration. 2010-2013
+ *  See  http://www.eu-emi.eu/partners for details on the copyright
+ *  holders.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "gfal2_space.h"
+
+#include <json.h>
+#include <string.h>
+
+
+static const char *retention2str(enum space_retention_policy policy)
+{
+    switch (policy) {
+        case GFAL_POLICY_REPLICA:
+            return "REPLICA";
+        case GFAL_POLICY_OUTPUT:
+            return "OUTPUT";
+        case GFAL_POLICY_CUSTODIAL:
+            return "CUSTODIAL";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+
+static const char *accesslatency2str(enum space_latency latency)
+{
+    switch (latency) {
+        case GFAL_LATENCY_ONLINE:
+            return "ONLINE";
+        case GFAL_LATENCY_NEARLINE:
+            return "NEARLINE";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+size_t gfal2_space_generate_json(struct space_report *report, char *buffer, size_t s_buff)
+{
+    struct json_object *root = json_object_new_object();
+
+    if (report->spacetoken) {
+        json_object_object_add(root, "spacetoken", json_object_new_string(report->spacetoken));
+    }
+    if (report->owner) {
+        json_object_object_add(root, "owner", json_object_new_string(report->owner));
+    }
+
+    json_object_object_add(root, "totalsize", json_object_new_int64(report->total));
+    json_object_object_add(root, "unusedsize", json_object_new_int64(report->free));
+    json_object_object_add(root, "usedsize", json_object_new_int64(report->used));
+
+    if (report->largest_chunk) {
+        json_object_object_add(root, "guaranteedsize", json_object_new_int64(*report->largest_chunk));
+    }
+
+    if (report->lifetime_assigned) {
+        json_object_object_add(root, "totalsize", json_object_new_int(*report->lifetime_assigned));
+    }
+    if (report->lifetime_left) {
+        json_object_object_add(root, "lifetimeleft", json_object_new_int(*report->lifetime_left));
+    }
+    if (report->retention) {
+        json_object_object_add(root, "retention",
+            json_object_new_string(retention2str(*report->retention)));
+    }
+    if (report->latency) {
+        json_object_object_add(root, "accesslatency",
+            json_object_new_string(accesslatency2str(*report->latency)));
+    }
+
+    const char *serialized = json_object_to_json_string(root);
+    strncpy(buffer, serialized, s_buff);
+
+    json_object_put(root);
+
+    return strlen(buffer);
+}
---
+++ src/utils/space/gfal2_space.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) CERN 2016
+ *
+ * Copyright (c) Members of the EMI Collaboration. 2010-2013
+ *  See  http://www.eu-emi.eu/partners for details on the copyright
+ *  holders.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GFAL_SPACE_H_
+#define GFAL_SPACE_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*
+ * Utilities for space reporting via extended attributes
+ */
+
+enum space_latency {
+    GFAL_LATENCY_UNKNOWN = 0,
+    GFAL_LATENCY_ONLINE,
+    GFAL_LATENCY_NEARLINE
+};
+
+enum space_retention_policy {
+    GFAL_POLICY_UNKNOWN = 0,
+    GFAL_POLICY_REPLICA,
+    GFAL_POLICY_OUTPUT,
+    GFAL_POLICY_CUSTODIAL
+};
+
+struct space_report {
+    uint64_t used;
+    uint64_t free;
+    uint64_t total;
+
+    // Optional values
+    uint64_t *largest_chunk;
+    int *lifetime_assigned;
+    int *lifetime_left;
+
+    enum space_latency *latency;
+    enum space_retention_policy *retention;
+
+    char *owner;
+    char *spacetoken;
+};
+
+/*
+ * Generate a json containing the information in space_report.
+ * Optional fields set to NULL are skipped.
+ */
+size_t gfal2_space_generate_json(struct space_report *report, char *buffer, size_t s_buff);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // GFAL_SPACE_H_
