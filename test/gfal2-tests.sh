1b1ff52d937aeaf16a8770201acd6619d97daed9
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2016-10-27 15:35:52 +0200

    Add Docker for tests

---
+++ test/gfal2-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+set -ex
+
+export HOME=/
+
+VOMS=${VOMS:=dteam}
+PASSWD=$1
+
+if [[ -z "$PASSWD" ]]; then
+    echo "Missing password"
+    exit 1
+fi
+
+cat > /etc/gfal2.d/sftp_plugin.conf <<EOF
+[SFTP PLUGIN]
+PASSPHRASE=${PASSWD}
+EOF
+
+echo "${PASSWD}" | voms-proxy-init --rfc --voms "${VOMS}" --pwstdin
+cd /usr/share/gfal2/tests
+ctest -T test 
+
