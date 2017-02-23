a8a64e16427ec5a718bd77bcdbf80abe96de995e
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2017-02-23 09:35:59 +0100

    Update copyright notice

--- test/common/gfal_lib_test.h
+++ test/common/gfal_lib_test.h
@@ -1,23 +1,26 @@
-#pragma once
-#ifndef GFAL_LIB_TEST_H
-#define GFAL_LIB_TEST_H
-
 /*
- * Copyright (c) Members of the EGEE Collaboration. 2004.
- * See http://www.eu-egee.org/partners/ for details on the copyright holders.
+ * Copyright (c) CERN 2013-2017
+ *
+ * Copyright (c) Members of the EMI Collaboration. 2010-2013
+ *  See  http://www.eu-emi.eu/partners for details on the copyright
+ *  holders.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#pragma once
+#ifndef GFAL_LIB_TEST_H
+#define GFAL_LIB_TEST_H
 
 #include <gfal_api.h>
 
@@ -50,4 +53,4 @@ int clean_file(const char* surl);
 }
 #endif
 
-#endif /* GFAL_LIB_TEST_H */ 
+#endif /* GFAL_LIB_TEST_H */
--- test/stress-test/gfalt_copyfile_fts_style_load_test.c
+++ test/stress-test/gfalt_copyfile_fts_style_load_test.c
@@ -1,6 +1,23 @@
-/**
- * Compile command : gcc -o gfalt_copyfile gfalt_copyfile.c `pkg-config --libs --cflags gfal_transfer`
+/*
+ * Copyright (c) CERN 2013-2017
+ *
+ * Copyright (c) Members of the EMI Collaboration. 2010-2013
+ *  See  http://www.eu-emi.eu/partners for details on the copyright
+ *  holders.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 
 #include <stdio.h>
 #include <time.h>
@@ -66,7 +83,7 @@ int main(int argc, char** argv){
 	}
 	GError * tmp_err = NULL; // classical GError/glib error management
 	gfal2_context_t handle;
-	
+
 	// initialize gfal
     gfal_set_verbose(GFAL_VERBOSE_TRACE | GFAL_VERBOSE_VERBOSE | GFAL_VERBOSE_DEBUG);
 
