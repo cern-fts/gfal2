adba45c1a2ad80213bb7b5850fc3fa5e6fff3ac1
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2017-07-13 16:47:23 +0200

    Avoid double slash for tests

--- test/common/gfal_lib_test.c
+++ test/common/gfal_lib_test.c
@@ -20,14 +20,11 @@
 
 
 #include <execinfo.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <pthread.h>
 #include <signal.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/utsname.h>
 #include <unistd.h>
 
@@ -40,8 +37,16 @@ char* generate_random_uri(const char* uri_dir, const char* prefix, char* buff,
     struct utsname info;
     memset(&info, 0, sizeof(struct utsname));
     uname(&info);
-    snprintf(buff, s_buff, "%s/%s_%s_%d%ld%ld", uri_dir, info.nodename, prefix, (int) getpid(),
+
+    if (uri_dir[strlen(uri_dir)-1] != '/') {
+        snprintf(buff, s_buff, "%s/%s_%s_%d%ld%ld", uri_dir, info.nodename, prefix, (int) getpid(),
             (long) time(NULL), (long) rand());
+    }
+    else {
+        snprintf(buff, s_buff, "%s%s_%s_%d%ld%ld", uri_dir, info.nodename, prefix, (int) getpid(),
+            (long) time(NULL), (long) rand());
+    }
+
     return buff;
 }
 
