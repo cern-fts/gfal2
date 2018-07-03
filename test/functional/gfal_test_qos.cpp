b231dbd6d32cc72f5d89f2ece283d6ebe42b0595
Aris Angelogiannopoulos <aris.angelogiannopoulos@cern.ch>
2018-07-03 16:59:52 +0200

    Get the token from the environment

--- test/functional/gfal_test_qos.cpp
+++ test/functional/gfal_test_qos.cpp
@@ -33,21 +33,47 @@
 class QosTest: public testing::Test {
 public:
     static const char* root;
+    static const char *token;
 
     gfal2_context_t context;
+    gfal2_context_t handle;
+    gfalt_params_t params;
+    gfal2_cred_t *cred;
 
     QosTest() {
         GError *error = NULL;
         context = gfal2_context_new(&error);
+        cred = gfal2_cred_new(GFAL_CRED_BEARER, token);
         Gfal::gerror_to_cpp(&error);
     }
 
     virtual ~QosTest() {
+    	gfal2_cred_free(cred);
         gfal2_context_free(context);
     }
 
     virtual void SetUp() {
-        GError* error = NULL;
+    	unsetenv("X509_USER_PROXY");
+    	unsetenv("X509_USER_CERT");
+    	unsetenv("X509_USER_KEY");
+
+    	int ret;
+    	GError *error = NULL;
+
+    	Gfal::gerror_to_cpp(&error);
+    	params = gfalt_params_handle_new(&error);
+    	Gfal::gerror_to_cpp(&error);
+
+    	ret = gfal2_cred_clean(context, &error);
+    	ASSERT_PRED_FORMAT2(AssertGfalSuccess, ret, error);
+
+    	// Clear automatic setup
+    	gfal2_remove_opt(context, "X509", "CERT", NULL);
+    	gfal2_remove_opt(context, "X509", "KEY", NULL);
+
+    	// Set configured values
+    	ret = gfal2_cred_set(context, "dcache-xdc.desy.de", cred, &error);
+    	ASSERT_PRED_FORMAT2(AssertGfalSuccess, ret, error);
     }
 
     virtual void TearDown() {
@@ -55,15 +81,15 @@ public:
     }
 };
 
+const char *QosTest::root = NULL;
+const char *QosTest::token = NULL;
 
 TEST_F(QosTest, TestSimpleCase)
 {
 	GError *err = NULL;
-	gfal2_qos_check_classes(context, "https://dcache-xdc.desy.de:6443", "dataobject", &err);
+	gfal2_qos_check_classes(context, root, "dataobject", &err);
 	EXPECT_EQ(NULL, err);
 }
-
-const char* QosTest::root;
 
 int main(int argc, char** argv)
 {
@@ -76,6 +102,7 @@ int main(int argc, char** argv)
     }*/
 
     /*QosTest::root = argv[1];*/
-
+    QosTest::root = "https://dcache-xdc.desy.de:6443";
+    QosTest::token = "eyJraWQiOiJyc2ExIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJmZWE1ZTZlMi0wYjlmLTQwZjUtYjE5OC00YmI3YWU0YjIzNGEiLCJpc3MiOiJodHRwczpcL1wvaWFtLmV4dHJlbWUtZGF0YWNsb3VkLmV1XC8iLCJleHAiOjE1MzA2MjAxMTksImlhdCI6MTUzMDYxNjUxOSwianRpIjoiODU0ODdhNDQtZWRlNS00NzcxLTg0MTctYjJkMTM3YzAxNmExIn0.AH-epaG5325e8Sq4X5oEe9-otB6fB_-JDRr6A7D5lLu1AHqThgWdT7Elr9T7Q-T6-d7UYjhy0xuoS_ojavjMu6OPnss1YvDZyVj6gny_ihtjgI5h6VNe-x8Jqbj-hyAijuMLdNNRbnBf9NeZEnyFBrJmLQrJP3pJUMk83y6xmO4";
     return RUN_ALL_TESTS();
 }
