f80c7d6588181502c32d323798b052194f85c5ea
Andrea Manzi <amanzi@cern.ch>
2018-10-31 15:45:18 +0100

    DMC-1117: calculate the checksum for file:// within the gridftp plugin

--- src/plugins/gridftp/gridftp_ns_checksum.cpp
+++ src/plugins/gridftp/gridftp_ns_checksum.cpp
@@ -35,18 +35,25 @@ extern "C" int gfal_gridftp_checksumG(plugin_handle handle, const char* url,
         off_t start_offset, size_t data_length, GError ** err)
 {
     g_return_val_err_if_fail(handle != NULL && url != NULL, -1, err,
-            "[gfal_gridftp_checksumG][gridftp] Invalid parameeters");
+            "[gfal_gridftp_checksumG][gridftp] Invalid parameters");
 
     GError * tmp_err = NULL;
     int ret = -1;
-    gfal2_log(G_LOG_LEVEL_DEBUG, "  -> [gfal_gridftp_checksumG]");
-    CPP_GERROR_TRY
-        (static_cast<GridFTPModule*>(handle))->checksum(url, check_type,
-                checksum_buffer, buffer_length, start_offset,
-                data_length);
-        ret = 0;
-    CPP_GERROR_CATCH(&tmp_err);
-    gfal2_log(G_LOG_LEVEL_DEBUG, "  [gfal_gridftp_checksumG] <-");
+
+    if (strncmp(url, "file:", 4) == 0 ){
+    	gfal2_log(G_LOG_LEVEL_DEBUG, "  -> [gfal_gridftp_file_checksumG]");
+    	ret = gfal2_checksum((gfal2_context_t) handle, url, check_type, 0, 0, checksum_buffer, buffer_length, &tmp_err);
+    	gfal2_log(G_LOG_LEVEL_DEBUG, "  [gfal_gridftp_file_checksumG] <-");
+    } else {
+		gfal2_log(G_LOG_LEVEL_DEBUG, "  -> [gfal_gridftp_checksumG]");
+		CPP_GERROR_TRY
+			(static_cast<GridFTPModule*>(handle))->checksum(url, check_type,
+					checksum_buffer, buffer_length, start_offset,
+					data_length);
+			ret = 0;
+		CPP_GERROR_CATCH(&tmp_err);
+		gfal2_log(G_LOG_LEVEL_DEBUG, "  [gfal_gridftp_checksumG] <-");
+    }
     G_RETURN_ERR(ret, tmp_err, err);
 }
 
@@ -59,31 +66,32 @@ void GridFTPModule::checksum(const char* url, const char* check_type,
     gfal2_log(G_LOG_LEVEL_DEBUG, " Checksum calculation %s for url %s",
             check_type, url);
 
-    GridFTPSessionHandler handler(_handle_factory, url);
-    GridFTPRequestState req(&handler, GRIDFTP_REQUEST_FTP);
+
+	GridFTPSessionHandler handler(_handle_factory, url);
+	GridFTPRequestState req(&handler, GRIDFTP_REQUEST_FTP);
 
     if (buffer_length < 16) {
-        throw Gfal::CoreException(GFAL_GRIDFTP_SCOPE_CHECKSUM, ENOBUFS,
-                "buffer length for checksum calculation is not enough");
-    }
+		throw Gfal::CoreException(GFAL_GRIDFTP_SCOPE_CHECKSUM, ENOBUFS,
+					"buffer length for checksum calculation is not enough");
+	}
 
-    globus_result_t res = globus_ftp_client_cksm(req.handler->get_ftp_client_handle(),
-            url, req.handler->get_ftp_client_operationattr(),
-            checksum_buffer, start_offset,
-            ((data_length) ? (data_length) : (-1)), check_type,
-            globus_ftp_client_done_callback, &req);
-    gfal_globus_check_result(GFAL_GRIDFTP_SCOPE_CHECKSUM, res);
-    // wait for answer with a timeout
-    const time_t global_timeout = gfal2_get_opt_integer_with_default(
-        _handle_factory->get_gfal2_context(),
-        CORE_CONFIG_GROUP, CORE_CONFIG_CHECKSUM_TIMEOUT, 1800
-    );
+	globus_result_t res = globus_ftp_client_cksm(req.handler->get_ftp_client_handle(),
+			url, req.handler->get_ftp_client_operationattr(),
+			checksum_buffer, start_offset,
+			((data_length) ? (data_length) : (-1)), check_type,
+			globus_ftp_client_done_callback, &req);
+	gfal_globus_check_result(GFAL_GRIDFTP_SCOPE_CHECKSUM, res);
+	// wait for answer with a timeout
+	const time_t global_timeout = gfal2_get_opt_integer_with_default(
+		_handle_factory->get_gfal2_context(),
+	    CORE_CONFIG_GROUP, CORE_CONFIG_CHECKSUM_TIMEOUT, 1800
+	);
 
-    const time_t timeout = gfal2_get_opt_integer_with_default(
-            _handle_factory->get_gfal2_context(),
-            GRIDFTP_CONFIG_GROUP, gridftp_checksum_calc_timeout, global_timeout);
+	const time_t timeout = gfal2_get_opt_integer_with_default(
+			_handle_factory->get_gfal2_context(),
+			GRIDFTP_CONFIG_GROUP, gridftp_checksum_calc_timeout, global_timeout);
 
+	req.wait(GFAL_GRIDFTP_SCOPE_CHECKSUM, timeout);
 
-    req.wait(GFAL_GRIDFTP_SCOPE_CHECKSUM, timeout);
-    gfal2_log(G_LOG_LEVEL_DEBUG, " <- [GridFTPModule::checksum] ");
+	gfal2_log(G_LOG_LEVEL_DEBUG, " <- [GridFTPModule::checksum] ");
 }
