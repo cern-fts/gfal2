c4189ea893399b4c7459f05b98cb40761ebc0460
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2021-05-18 13:15:40 +0200

    Initiate GitLab-CI pipeline

---
+++ ci/common-rpm-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+cp -v ci/repo/dmc-devel-el7.repo /etc/yum.repos.d/
+
+TIMESTAMP=`date +%y%m%d%H%M`
+GITREF=`git rev-parse --short HEAD`
+RELEASE=r${TIMESTAMP}git${GITREF}
+
+RPMBUILD=${PWD}/build
+SRPMS=${RPMBUILD}/SRPMS
+
+cd packaging/
+make srpm RELEASE=${RELEASE} RPMBUILD=${RPMBUILD} SRPMS=${SRPMS}
+
+if [[ -f /usr/bin/dnf ]]; then
+  dnf install -y epel-release || true
+  dnf builddep -y ${SRPMS}/*
+else
+  yum-builddep -y ${SRPMS}/*
+fi
+
+rpmbuild --rebuild --define="_topdir ${RPMBUILD}" ${SRPMS}/*
