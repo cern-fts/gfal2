bfe518b2394b1d2a5e72cbc8003d025487db4bb3
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2021-09-14 10:45:55 +0200

    DMC-1264: Accommodate Fedora builds - Modify common-rpm-build script to account also for Fedora builds - Add Fedora repository files

--- ci/common-rpm-build.sh
+++ ci/common-rpm-build.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=`rpm --eval %{centos_ver}`
-
-cp -v ci/repo/dmc-devel-el${VERSION}.repo /etc/yum.repos.d/
-
 TIMESTAMP=`date +%y%m%d%H%M`
 GITREF=`git rev-parse --short HEAD`
 BRANCH=`git name-rev $GITREF --name-only`
 RELEASE=r${TIMESTAMP}git${GITREF}
 
+BUILD="devel"
+DIST=$(rpm --eval "%{dist}" | cut -d. -f2)
+
+# Special handling of FC rawhide
+[[ "${DIST}" == "fc36" ]] && DIST="fc-rawhide"
+
 if [[ $BRANCH == tags/* ]]; then
   RELEASE=
+  BUILD="rc"
+fi
+
+REPO_FILE="dmc-${BUILD}-${DIST}.repo"
+
+if [[ -f "ci/repo/${REPO_FILE}" ]]; then
+  cp -v "ci/repo/${REPO_FILE}" "/etc/yum.repos.d/"
 fi
 
 RPMBUILD=${PWD}/build
