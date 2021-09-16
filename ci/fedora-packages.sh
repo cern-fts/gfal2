1639f63c41215482d1a96789cffe217189fcbe85
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2021-09-16 10:09:40 +0200

    CI: List artifacts directory at the end of build jobs

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -2,9 +2,9 @@
 set -e
 
 if [[ -f /usr/bin/dnf ]]; then
-  dnf install -y dnf-plugins-core git rpm-build which python2 \
+  dnf install -y dnf-plugins-core git rpm-build tree which python2 \
                  cmake cmake3 make gcc gcc-c++
 else
-  yum install -y yum-utils git rpm-build make which python2 \
+  yum install -y yum-utils git rpm-build make tree which python2 \
                  cmake cmake3 make gcc gcc-c++
 fi
