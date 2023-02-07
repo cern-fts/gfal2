f2cc55e15d7e36d0598ed5c0fe1bca59279a29ce
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-02-07 17:43:19 +0100

    Update rawhide version on gfal2-repo-manager.py

--- packaging/gfal2-repo-manager.py
+++ packaging/gfal2-repo-manager.py
@@ -11,7 +11,7 @@ import sys
 
 DRY_RUN = False
 NO_CREATE_REPO = False
-RAWHIDE_VERSIONS = ["fc37", "fc38"]
+RAWHIDE_VERSIONS = ["fc38", "fc39"]
 
 def sh(cmd):
     # poor man's subprocess.check_output, not supported on SL6
